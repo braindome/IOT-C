803bd5d52404fb10b5eaab029c8de9a2e954dbde
Antonio Saracino <antonio.saracino83@gmail.com>
2025-10-01 16:49:07 +0200

    10-01

---
+++ CDEMO1001/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {  
+
+  int jersey;
+  printf("What jerseu did Foppa use?");
+  scanf(" %d", &jersey);
+
+  // char bokstaven = 'A';
+  // printf("Bokstaven är: %c, vad kul\n", bokstaven);
+  // printf("Bokstaven är: %d, vad kul\n", bokstaven);
+
+  // float num = 12.31567;
+  // printf("Värdet är %.3f hej hej\n", num);
+  return 0;
+}
